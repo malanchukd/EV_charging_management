81e71b5f2ee061c09dc6a39b4f577e39c1c37b6c
Dmytro Malanchuk <umalanch@gmail.com>
2026-03-13 22:19:18 +0200

    edit liquibase and rename folder)

--- src/main/java/com/pet/project/ProjectApplication.java
+++ src/main/java/com/pet/project/ProjectApplication.java
@@ -1,4 +1,4 @@
-package com.pet.prorejct;
+package com.pet.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/pet/project/ServletInitializer.java
+++ src/main/java/com/pet/project/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.pet.prorejct;
+package com.pet.project;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
